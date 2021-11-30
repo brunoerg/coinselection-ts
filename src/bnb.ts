cc829dcbff07140506c9bfe704e7fc7dcb661bb1
Bruno Garcia <brunoely.gc@gmail.com>
2021-11-30 11:10:05 -0200

    bnb: use GetSelectionAmount for values

--- src/bnb.ts
+++ src/bnb.ts
@@ -1,3 +1,4 @@
+import * as utils from "./utils";
 import { Input } from "./types/input.type";
 import { OutputGroup } from "./types/output_group.type";
 
@@ -26,11 +27,11 @@ export function bnb(utxos: Array<OutputGroup>, selection_target: number, cost_of
         let backtrack: boolean = false;
         if (curr_value + curr_available_value < selection_target ||
             curr_value > selection_target + cost_of_change ||
-            //(curr_waste > best_waste && (utxos) > 0)) {
             (curr_waste > best_waste && (utxos[0].fee - utxos[0].long_term_fee) > 0)) {
             backtrack = true;
         } else if (curr_value >= selection_target) {
             curr_waste += (curr_value - selection_target);
+            
             if (curr_waste <= best_waste) {
                 best_selection = curr_selection;
                 best_waste = curr_waste;
@@ -38,6 +39,7 @@ export function bnb(utxos: Array<OutputGroup>, selection_target: number, cost_of
                     break;
                 }
             }
+
             curr_waste -= (curr_value - selection_target);
             backtrack = true;
         }
@@ -45,7 +47,7 @@ export function bnb(utxos: Array<OutputGroup>, selection_target: number, cost_of
         if (backtrack) {
             while (curr_selection.length > 0 && !curr_selection[curr_selection.length - 1]) {
                 curr_selection.pop();
-                curr_available_value += utxos[utxos.length - 1].value;
+                curr_available_value += utils.getSelectionAmount(true, utxos[utxos.length - 1]);
             }
 
             if (curr_selection.length == 0) {
@@ -54,19 +56,19 @@ export function bnb(utxos: Array<OutputGroup>, selection_target: number, cost_of
 
             curr_selection[curr_selection.length - 1] = false;
             let utxo: OutputGroup = utxos[curr_selection.length - 1];
-            curr_value -= utxo.value;
+            curr_value -= utils.getSelectionAmount(true, utxo);
             curr_waste -= utxo.fee - utxo.long_term_fee;
         } else {
             let utxo: OutputGroup = utxos[curr_selection.length];
-            curr_available_value -= utxo.value;
+            curr_available_value -= utils.getSelectionAmount(true, utxo);
 
             if (curr_selection.length > 0 && !curr_selection[curr_selection.length - 1] &&
-                utxo.value == utxos[curr_selection.length - 1].value &&
+                utils.getSelectionAmount(false, utxo) == utils.getSelectionAmount(false, utxos[curr_selection.length - 1]) &&
                 utxo.fee == utxos[curr_selection.length - 1].fee) {
                 curr_selection.push(false);
             } else {
                 curr_selection.push(true);
-                curr_value += utxo.value;
+                curr_value += utils.getSelectionAmount(true, utxo);
                 curr_waste += utxo.fee - utxo.long_term_fee;
             }
         }
